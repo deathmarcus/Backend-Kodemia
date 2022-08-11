f626f92aed3411a43e9cbe36dbacecac432fb2d1
Marco Castañeda <marco.dcc@gmail.com>
2022-08-10 19:28:45 -0500

    Ejercicios clase 2

--- filesystem.js
+++ filesystem.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
-fs.writeFile("text.txt", "Archivo creado desde node", "utf8", (err) => {
+// fs.writeFile("text.txt", "Archivo creado desde node", "utf8", (err) => {
+//     if(err) {
+//         console.log("err", err);
+//         return
+//     };
+//     console.log("El Archivo fue creado con exito");
+// });
+
+fs.readFile("text.txt", 'utf8', (err, content) => {
     if(err) {
-        console.log("err", err);
+        console.log("error", err);
         return
     };
-    console.log("El Archivo fue creado con exito");
+    console.log(content);
 });
